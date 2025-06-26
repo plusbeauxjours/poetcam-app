12446e8f528fc2127e368779bf95a1800934a9d6
plusbeauxjours <plusbeauxjours@gmail.com>
2025-06-26 22:12:44 +0900

    WIP: router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack initialRouteName="splash">
-          <Stack.Screen name="splash" options={{ headerShown: false }} />
-          <Stack.Screen name="camera" options={{ headerShown: false }} />
-          <Stack.Screen name="result" options={{ headerShown: false }} />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
-    </GestureHandlerRootView>
+    <Stack initialRouteName="splash">
+      <Stack.Screen name="splash" options={{ headerShown: false }} />
+      <Stack.Screen name="camera" options={{ headerShown: false }} />
+    </Stack>
   );
 }
--- app/splash.tsx
+++ app/splash.tsx
@@ -1,26 +1,27 @@
-import { useCameraPermissions } from "expo-camera";
-import * as ImagePicker from "expo-image-picker";
 import { router } from "expo-router";
 import { useEffect } from "react";
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
 export default function SplashScreen() {
-  const [, requestCameraPermission] = useCameraPermissions();
-  const [, requestMediaPermission] = ImagePicker.useMediaLibraryPermissions();
-
   useEffect(() => {
-    (async () => {
-      await requestCameraPermission();
-      await requestMediaPermission();
-      setTimeout(() => {
-        router.replace("/camera");
-      }, 500);
-    })();
+    const timer = setTimeout(() => {
+      router.replace("/camera");
+    }, 1000); // 1초 후 카메라 화면으로 이동
+
+    return () => clearTimeout(timer);
   }, []);
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={styles.container}>
       <ActivityIndicator size="large" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
