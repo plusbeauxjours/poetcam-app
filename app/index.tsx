550a671364854b7f9bdbe2fc354b9160c29d5aa6
plusbeauxjours <plusbeauxjours@gmail.com>
2025-06-28 23:57:37 +0900

    set: color pallete

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,15 @@
+import { Colors } from "@/constants/Colors";
 import { CameraView, useCameraPermissions } from "expo-camera";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function CameraScreen() {
   const [permission, requestPermission] = useCameraPermissions();
 
   if (!permission) {
-    // Camera permissions are still loading.
     return <View />;
   }
 
   if (!permission.granted) {
-    // Camera permissions are not granted yet.
     return (
       <View style={styles.container}>
         <Text style={{ textAlign: "center" }}>We need your permission to show the camera</Text>
@@ -18,10 +17,17 @@ export default function CameraScreen() {
       </View>
     );
   }
-  console.log("permission", permission);
+
   return (
     <View style={styles.container}>
       <CameraView style={styles.camera} facing="back" />
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={1}
+        onPress={() => {
+          console.log("take photo");
+        }}
+      />
     </View>
   );
 }
@@ -33,5 +39,16 @@ const styles = StyleSheet.create({
   },
   camera: {
     flex: 1,
+  },
+  button: {
+    position: "absolute",
+    width: 100,
+    height: 100,
+    backgroundColor: Colors.grey[500],
+    borderRadius: 100,
+    bottom: 100,
+    left: "50%",
+    right: "50%",
+    transform: [{ translateX: -50 }],
   },
 });
