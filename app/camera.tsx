225d9a0f85f63897f0a7d679ac42f9ae4d17e932
plusbeauxjours <plusbeauxjours@gmail.com>
2025-07-09 10:33:38 +0900

    set: camera screen

--- app/camera.tsx
+++ app/camera.tsx
@@ -1,36 +1,19 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { CameraView, useCameraPermissions } from "expo-camera";
-import * as MediaLibrary from "expo-media-library";
-import { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useRef, useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function CameraScreen() {
   const [permission, requestPermission] = useCameraPermissions();
-  const [mediaPermission, setMediaPermission] = useState<MediaLibrary.PermissionResponse | null>(
-    null
-  );
+  const [type, setType] = useState<"front" | "back">("back");
+  const [flash, setFlash] = useState<"off" | "on" | "auto">("off");
+  const cameraRef = useRef(null);
 
-  useEffect(() => {
-    (async () => {
-      const status = await MediaLibrary.requestPermissionsAsync();
-      setMediaPermission(status);
-    })();
-  }, []);
-
-  if (mediaPermission && !mediaPermission.granted) {
-    return (
-      <View style={styles.container}>
-        <Text style={{ textAlign: "center" }}>
-          We need your permission to access the media library
-        </Text>
-        <Button
-          onPress={() => MediaLibrary.requestPermissionsAsync().then(setMediaPermission)}
-          title="grant media permission"
-        />
-      </View>
-    );
+  if (!permission) {
+    return <View />;
   }
 
-  if (permission && !permission.granted) {
+  if (!permission.granted) {
     return (
       <View style={styles.container}>
         <Text style={{ textAlign: "center" }}>We need your permission to show the camera</Text>
@@ -38,10 +21,29 @@ export default function CameraScreen() {
       </View>
     );
   }
-  console.log("permission", permission);
+
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing="back" />
+      <CameraView ref={cameraRef} style={styles.camera} facing={type} flash={flash} />
+      <View style={styles.controls}>
+        <TouchableOpacity onPress={() => setType(type === "back" ? "front" : "back")}>
+          <MaterialCommunityIcons name="camera-switch" size={32} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity /* onPress={handleTakePicture} */>
+          <MaterialCommunityIcons name="camera" size={48} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setFlash(flash === "off" ? "on" : flash === "on" ? "auto" : "off")}>
+          <MaterialCommunityIcons
+            name={flash === "off" ? "flash-off" : flash === "on" ? "flash" : "flash-auto"}
+            size={32}
+            color="white"
+          />
+        </TouchableOpacity>
+        {/* <TouchableOpacity onPress={handleOpenGallery}>
+          <MaterialCommunityIcons name="image-multiple" size={32} color="white" />
+        </TouchableOpacity> */}
+      </View>
     </View>
   );
 }
@@ -50,8 +52,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    backgroundColor: "black",
   },
   camera: {
     flex: 1,
+  },
+  controls: {
+    position: "absolute",
+    bottom: 40,
+    left: 0,
+    right: 0,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
   },
 });
